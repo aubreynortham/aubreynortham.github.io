655ca728718f445e81061a639b0481e635000c03
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-07 00:43:47 -0400

    body copy filled in, contact functionality added

--- script.js
+++ script.js
@@ -17,5 +17,40 @@ $(document).ready(function() {
     sectionsColor: ['#d24c01', '#d24c01', '#d27f01', '#ec3333']
   });
 
+  // icon hover effects
+  $(".puz").hover(function(){
+    $("#altFaves").html(" puzzles.");
+    $("#altFaves").toggle("slow");
+  });
+  $(".cat").hover(function(){
+    $("#altFaves").html(" cats, napping, & napping cats.");
+    $("#altFaves").toggle("slow");
+  });
+  $(".row").hover(function(){
+    $("#altFaves").html(" rowing.");
+    $("#altFaves").toggle("slow");
+  });
+  $(".draw").hover(function(){
+    $("#altFaves").html(" sketching (mostly portraits).");
+    $("#altFaves").toggle("slow");
+  });
+  $(".can").hover(function(){
+    $("#altFaves").html(" trying to garden.");
+    $("#altFaves").toggle("slow");
+  });
+  $(".book").hover(function(){
+    $("#altFaves").html(" psychological thrillers.");
+    $("#altFaves").toggle("slow");
+  });
+  $(".piz").hover(function(){
+    $("#altFaves").html(" pizza.");
+    $("#altFaves").toggle("slow");
+  });
+  $(".bulb").hover(function(){
+    $("#altFaves").html(" going green.");
+    $("#altFaves").toggle("slow");
+  });
+
+
 
 });
