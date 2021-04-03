73b83769f77c1e2c23f41e3d90fa6b6b23505273
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2021-04-03 00:02:26 -0400

    Add pause mechanism

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// console sauce
 const style = "font-size: 20px;" +
   "background: #355C7D;" +
   "background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);" +
@@ -7,7 +8,21 @@ const style = "font-size: 20px;" +
   "padding: 10px 15px;" +
   "width: 100%;" +
   "border-radius: 32px;";
-
 const text = "%cnothing to see here 👀";
-
 console.log(text, style);
+
+// video controls
+const controlVideo = (el) => {
+  const video = document.getElementById("theVid");
+  if (video) {
+    if (video.paused) {
+      video.play();
+      video.style.filter = 'none';
+      el.innerHTML = 'enough clouds thx';
+    } else {
+      video.pause();
+      video.style.filter = 'invert(0.8)';
+      el.innerHTML = 'more clouds plz';
+    }
+  }
+};
