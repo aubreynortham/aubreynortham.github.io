6712b7a458b6480c4f734ca010a85f2ceeafdaeb
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2021-04-02 22:22:14 -0400

    Console sauce

---
+++ index.js
@@ -0,0 +1,13 @@
+const style = "font-size: 20px;" +
+  "background: #355C7D;" +
+  "background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);" +
+  "background: linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);" +
+  "color: white;" +
+  "text-align: center;" +
+  "padding: 10px 15px;" +
+  "width: 100%;" +
+  "border-radius: 32px;";
+
+const text = "%cnothing to see here 👀";
+
+console.log(text, style);
